5c10d050278274f4bee5f30883a85dbddcb31d9a
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-08-20 15:22:40 +0900

    solve: Add Two Integers

---
+++ 2235.add-two-integers.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2235 lang=golang
+ *
+ * [2235] Add Two Integers
+ */
+
+// @lc code=start
+func sum(num1 int, num2 int) int {
+	n1 := num1
+	n2 := num2
+	return n1 + n2
+}
+
+// @lc code=end
+
