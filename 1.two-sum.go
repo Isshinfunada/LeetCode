6355ce86be4434ced0a439d7ba03c70c3c0b4d6a
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-08-22 00:48:03 +0900

    resubmit: mapを使った実装に変更してみた

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -7,36 +7,16 @@
 // @lc code=start
 package main
 
-type element struct {
-	position int
-	ok       bool
-}
-
 func twoSum(nums []int, target int) []int {
-	var tmp element
-	result := make([]int, 0)
-	i := -1
-	for !tmp.ok && (i < len(nums)) {
-		i++
-		n := target - nums[i]
-		tmp = inFunctionWithMap(nums, n)
-		if i == tmp.position {
-			tmp.ok = false
+	hm := make(map[int]int)
+	for i, num := range nums {
+		_, ok := hm[num]
+		if ok {
+			return []int{i, hm[num]}
 		}
+		hm[target-num] = i
 	}
-	result = append(result, i, tmp.position)
-	return result
-}
-
-func inFunctionWithMap(slice []int, key int) element {
-	var result element
-	for i, num := range slice {
-		if num == key {
-			result.position = i
-			result.ok = true
-		}
-	}
-	return result
+	return nil
 }
 
 // @lc code=end
