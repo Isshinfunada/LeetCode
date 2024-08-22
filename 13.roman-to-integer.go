cb691c35804bf4059a222ed2c6c2687e009dbc42
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-08-22 11:30:58 +0900

    ModelAnswer: つまりこういうことっていう落とし込むアイデアに感服

--- 13.roman-to-integer.go
+++ 13.roman-to-integer.go
@@ -8,70 +8,28 @@
 package main
 
 func romanToInt(s string) int {
-	result := make([]int, 15)
-	result = append(result, 0)
+	var v, lv, cv int
+	h := map[uint8]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
 
-	hm := make(map[rune]int)
-	hm['I'] = 1
-	hm['V'] = 5
-	hm['X'] = 10
-	hm['L'] = 50
-	hm['C'] = 100
-	hm['D'] = 500
-	hm['M'] = 1000
-
-	for i, roman := range s {
-		if i > 0 {
-			switch roman {
-			case 'I':
-				result = append(result, hm['I'])
-			case 'V':
-				if result[i-1] == 1 {
-					result = append(result, 3)
-				} else {
-					result = append(result, hm['V'])
-				}
-			case 'X':
-				if result[i-1] == 1 {
-					result = append(result, 8)
-				} else {
-					result = append(result, hm['X'])
-				}
-			case 'L':
-				if result[i-1] == 10 {
-					result = append(result, 30)
-				} else {
-					result = append(result, hm['L'])
-				}
-			case 'C':
-				if result[i-1] == 10 {
-					result = append(result, 80)
-				} else {
-					result = append(result, hm['C'])
-				}
-			case 'D':
-				if result[i-1] == 100 {
-					result = append(result, 300)
-				} else {
-					result = append(result, hm['D'])
-				}
-			case 'M':
-				if result[i-1] == 100 {
-					result = append(result, 800)
-				} else {
-					result = append(result, hm['M'])
-				}
-			}
+	for i := len(s) - 1; i >= 0; i-- {
+		cv = h[s[i]]
+		if cv < lv {
+			v -= cv
 		} else {
-			result = append(result, hm[roman])
+			v += cv
 		}
+		lv = cv
 	}
 
-	sum := 0
-	for _, num := range result {
-		sum += num
-	}
-	return sum
+	return v
 }
 
 // @lc code=end
