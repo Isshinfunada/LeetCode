aef10989697e6c9b7e7d5152ff7b582cf704532b
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-08-29 22:01:49 +0900

    メモリオーバー。諦め

--- 202.happy-number.go
+++ 202.happy-number.go
@@ -7,19 +7,12 @@
 // @lc code=start
 package main
 
-import (
-	"fmt"
-)
-
 func isHappy(n int) bool {
 	var values []int
 	result := n
 	for result != 1 {
 		values = convert(result)
 		result = happyLoop(values)
-		fmt.Printf("values: %#v\n", values)
-		fmt.Printf("n: %#v\n", n)
-		fmt.Printf("result: %#v\n", result)
 	}
 	return true
 }
@@ -29,8 +22,6 @@ func happyLoop(value []int) int {
 	r := make(map[int]int)
 	for i, v := range value {
 		r[i] = v * v
-		fmt.Printf("i: %#v\n", i)
-		fmt.Printf("v: %#v\n", v)
 	}
 
 	sum := 0
@@ -42,14 +33,16 @@ func happyLoop(value []int) int {
 
 // 1桁ずつ分解
 func convert(intnum int) []int {
-	cnt := 0
 	i := intnum
-
-	for i >= 9 {
-		i = i - 10
-		cnt += 1
+	var res []int
+	for i != 1 {
+		for i > 9 {
+			res = append(res, i/10)
+		}
+		res = append(res, i%10)
 	}
-	return []int{cnt, i}
+
+	return res
 }
 
 // @lc code=end
