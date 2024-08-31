65a8a1115b110314ae0d162585bb6ee78316412d
Isshinfunada <isshin.funada@zuuonline.com>
2024-09-01 01:08:00 +0900

    こっちやった

--- 389.find-the-difference.go
+++ 389.find-the-difference.go
@@ -7,26 +7,20 @@
 // @lc code=start
 package main
 
-import "fmt"
-
 func findTheDifference(s string, t string) byte {
 	exists := make(map[rune]int)
 	added := make(map[rune]int)
 	for _, r := range s {
 		cnt := exists[r]
 		exists[r] = cnt + 1
-		fmt.Printf("exists: %#v\n", exists[r])
 	}
 
 	for _, value := range t {
 		cnt := added[value]
 		added[value] = cnt + 1
-		fmt.Printf("addeed: %#v\n", added[value])
 	}
 
 	for v, _ := range added {
-		fmt.Printf("exists: %#v\n", exists[v])
-		fmt.Printf("addeed: %#v\n", added[v])
 		if exists[v] != added[v] {
 			return byte(v)
 		}
