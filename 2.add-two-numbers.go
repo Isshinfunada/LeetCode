ddf8069141b56276cfe5e11d4f45b43c733fb09b
Isshinfunada <isshin.funada@zuuonline.com>
2024-09-05 09:36:20 +0900

    model: 生成AI君

--- 2.add-two-numbers.go
+++ 2.add-two-numbers.go
@@ -14,43 +14,34 @@
  */
 package main
 
-import "fmt"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	current1 := l1
-	current2 := l2
-	var result *ListNode
+	dummy := &ListNode{Val: 0}
+	current := dummy
 	up := 0
 
-	for current1 != nil || current2 != nil {
-		if current1 == nil {
-			current1 = &ListNode{Val: 0, Next: current1}
+	for l1 != nil || l2 != nil || up > 0 {
+		sum := up
+
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
 		}
-		if current2 == nil {
-			current2 = &ListNode{Val: 0, Next: current2}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
 		}
-		fmt.Printf("current1Val: %#v\n", current1.Val)
-		fmt.Printf("current2Val: %#v\n", current2.Val)
-		fmt.Printf("up: %#v\n", up)
-		result = &ListNode{Val: (current1.Val + current2.Val + up) % 10, Next: result}
-		if (current1.Val + current2.Val + up) >= 10 {
-			up = 1
-			if current1.Next == nil || current2.Next == nil {
-				result.Next.Val = up
-			}
-		} else {
-			up = 0
-		}
-		current1 = current1.Next
-		current2 = current2.Next
-		fmt.Printf("result: %#v\n", result)
+
+		current.Next = &ListNode{Val: sum % 10}
+		current = current.Next
+		up = sum / 10
 	}
-	return result
+
+	return dummy.Next
 }
 
 // @lc code=end
